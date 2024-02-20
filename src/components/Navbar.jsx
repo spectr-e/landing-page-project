1bfce64e9ee90f23ca402b46cd47748da08366ba
Josiah ⚡ <81918288+spectr-e@users.noreply.github.com>
2024-02-20 21:55:46 +0300

    feat: added assets and Navbar component!

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import { hamburgerMenu, close, logo } from '../assets/'
+const Navbar = () => {
+  return (
+    <div className='w-screen h-[80px] z-10 bg-white fixed drop-shadow-lg'>
+      <div className=''></div>
+    </div>
+  )
+}
+
+export default Navbar
